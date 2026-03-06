2e36dcf99972b01a02c54f7d2875bc99b2abf8c0
SeungJo <binsama0106@gmail.com>
2026-03-07 01:58:10 +0900

    [level 0] Title: 문자열 묶기, Time: 14.49 ms, Memory: 16.9 MB -BaekjoonHub

--- 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.py
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.py
@@ -1,9 +1,11 @@
 def solution(strArr):
-    dic = {}
+    answer = 0
+    
+    dix = {}
     
     for i in strArr:
         length = len(i)
+        
+        dix[length] = dix.get(length,0) + 1
     
-        dic[length] = dic.get(length,0) + 1
-    
-    return max(dic.values())
+    return max(dix.values())

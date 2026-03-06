5e3b2e964efb031b93d859c399b85fca4a0454ea
SeungJo <binsama0106@gmail.com>
2026-03-07 01:41:02 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 0.03 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.py
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.py
@@ -1,14 +1,14 @@
 def solution(rank, attendance):
     answer = 0
     attend = []
-    result = []
     for i, v in enumerate(attendance):
         if v :
-            attend.append(rank[i])
+            attend.append((rank[i],i))
     attend = sorted(attend)
     
-    for k in attend:
-        result.append(rank.index(k))
+    a = attend[0][1]
+    b = attend[1][1]
+    c = attend[2][1]
     
     
-    return 10000*result[0] + 100*result[1] + result[2]
+    return 10000*a + 100*b + c

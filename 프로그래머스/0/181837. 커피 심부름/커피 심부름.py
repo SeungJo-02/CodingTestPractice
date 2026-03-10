cc9c7f9823824d619297dccac1eb57ef47458ac5
SeungJo <binsama0106@gmail.com>
2026-03-10 12:15:07 +0900

    [level 0] Title: 커피 심부름, Time: 0.10 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/0/181837. 커피 심부름/커피 심부름.py
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.py
@@ -1,17 +1,6 @@
-def solution(order):
-    answer = 0
-    for i in order:
-        if 'americano' in i:
-            answer += 4500
-        elif 'latte' in i :
-            answer += 5000
-        elif 'anything' in i:
-            answer += 4500
-            
-    return answer
-
-
-
 # 아 4500, 라 5000 상관없이
 # 메뉴만 적으면 아이스 
-#아무거나 아아
+# 아무거나 아아
+def solution(order):
+
+    return sum([5000 if 'latte' in i else 4500 for i in order])

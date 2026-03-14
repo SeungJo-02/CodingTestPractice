ae44f81e3c1970b5d8fd2e84dbb428051fdb63ce
SeungJo <binsama0106@gmail.com>
2026-03-15 02:10:56 +0900

    [level 1] Title: 시저 암호, Time: 2.02 ms, Memory: 9.25 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.py
@@ -0,0 +1,14 @@
+def solution(s, n):
+    answer = ''
+    for i in s:
+        num = 0
+        if i.isupper():
+            num = (ord(i) - ord('A') + n) %26 + ord('A')
+            answer += chr(num)
+            
+        elif i.islower():
+            num = (ord(i) - ord('a') + n) %26 + ord('a')
+            answer += chr(num)
+        else:
+            answer += " "
+    return answer

a61aa4d65343cf6a3ed3dd545a35599969121090
SeungJo <binsama0106@gmail.com>
2026-03-21 01:50:23 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.02 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -1,8 +1,7 @@
 def solution(n, arr1, arr2):
     answer = []
-    for i in range(n):
-        bin_row = bin(arr1[i] | arr2[i])[2:].zfill(n)
+    for j in range(n):
+        bin_row = bin(arr1[j] | arr2[j])[2:].zfill(n)
         row = bin_row.replace("1","#").replace("0"," ")
         answer.append(row)
-        
     return answer

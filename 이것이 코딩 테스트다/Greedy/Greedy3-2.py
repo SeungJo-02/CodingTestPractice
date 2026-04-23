d77eff90f61345aeb4b1deb83ebfaaa48b68dd12
seungjo <seungjo.eren@gmail.com>
2026-04-23 12:53:16 +0900

    Greedy3-2

---
+++ 이것이 코딩 테스트다/Greedy/Greedy3-2.py
@@ -0,0 +1,13 @@
+#n은 행 m은열
+n,m = map(int, input().split())
+
+result = 0
+
+for card in range(n):#n행 만큼 반복
+    data = list(map(int, input().split()))
+    min_val = 10001
+    for a in data:
+        min_val = min(min_val, a)
+    result = max(result, min_val)
+
+print(result)

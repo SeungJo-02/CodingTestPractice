9192fb9cb778ef35d8b9adf2a98d07450f72d4d4
seungjo <seungjo.eren@gmail.com>
2026-04-23 13:12:32 +0900

    Greedy4-1

---
+++ 이것이 코딩 테스트다/Greedy/Greedy4-1.py
@@ -0,0 +1,17 @@
+#n은 숫자, k는 나눌 숫자
+n,k = map(int, input().split())
+
+count = 0
+
+while n >= k:
+    while n % k != 0:
+        n -= 1
+        count += 1
+    n //= k
+    count += 1
+
+while n > 1:
+    n -= 1
+    count += 1
+
+print(count)

89ebe11381a21742ef2c65ac3d2f1d9109f5c502
seungjo <seungjo.eren@gmail.com>
2026-04-29 15:25:42 +0900

    상하좌우 복습

--- 이것이 코딩 테스트다/Review/implement/impliment1.py
+++ 이것이 코딩 테스트다/Review/implement/impliment1.py
@@ -1,19 +1,22 @@
 n = int(input())
-data = input().split()
 
-x,y = 1,1 
+x , y = 1, 1
 
-dy = [-1,1,0,0]
+plans = input().split()
+
+commends = ["L","R","U","D"]
 dx = [0,0,-1,1]
-commend = ["L","R","U","D"]
+dy = [-1,1,0,0]
 
-for i in data:
-    for k in range(len(commend)):
-        if commend[k] == i:
+for i in plans:
+    nx = x
+    ny = y
+    for k in range(len(commends)):
+        if i == commends[k]:
             nx = x + dx[k]
             ny = y + dy[k]
-    if nx < 1 or ny < 1 or nx > n or ny > n :
+
+    if nx < 1 or ny < 1 or nx > n or ny > n:
         continue
-    x = nx
-    y = ny
-print(x, y) 
+    x, y = nx, ny
+print(x,y)    

0d92f1462b0fd07e60965ec213603cc5dd799210
seungjo <seungjo.eren@gmail.com>
2026-05-07 17:01:52 +0900

    계수 정렬

---
+++ 이것이 코딩 테스트다/Sort/sort5.py
@@ -0,0 +1,12 @@
+#계수 정렬
+array = [7,5,9,0,3,1,6,2,9,1,4,8,0,5,2]
+#모든 범위의 데이터 
+count = [0] * (max(array) +1)
+#array 속 숫자가 몇 개 있는 지 숫자 센다
+for i in range(len(array)):
+    count[array[i]] += 1
+#출력   
+for i in range(len(count)):
+    for j in range(count[i]):
+        print(i, end = " ")
+
